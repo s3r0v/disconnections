b2ceb738a984098ce9476214c639968483ff2734
uebermensch1488 <lib.ballmerpeak@gmail.com>
2020-12-30 23:59:00 +0000

    bot

--- rep/tg.py
+++ rep/tg.py
@@ -1,6 +1,5 @@
 import telebot
 bot = telebot.TeleBot('1499593962:AAG5ObbcZsycM9O6M9wHD91Xi05b_mPsbTg')
 
-@bot.message_handler()
-def send_data(message):
-    bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+def send_data():
+	bot.send_message(1340073704, 'Wake up!')
