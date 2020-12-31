1c3cfdc4061201b75bd46cff885d37d2a866e493
Akhmetov Aslan <lib.ballmerpeak@gmail.com>
2020-12-31 20:41:27 +0300

    nigger

--- rep/main.py
+++ rep/main.py
@@ -2,21 +2,31 @@
 # Вся логика проекта описана здесь
 
 import time
+import sqlite3
 
 from req import get_html
 from parser import parse
 from date import get_date
-from db import create_database
-from tg import send_data
+from db import create_table, send_data_to_db
+from tg import send_data_to_tg
+
+
 
 def main():
+    sent_data_tg = [] # Данные об отключениях, которые были отправлены в телеграм
+    sent_data_db = [] # Данные об отключениях, которые были отправлены в базу данных 
+    create_table()
+
     while True:
         date = get_date()
         url = f"https://www.mrsk-1.ru/ajax/disconn_list.php?region=67&district=55DE65CD-3A62-4304-BD72-75CB153B8F33&start={date[0][0]}.{date[0][1]}.{date[0][2]}&end={date[1][0]}.{date[1][1]}.{date[1][2]}"
+        
         html = get_html(url)  # GET запрос для получения данных об отключениях
         results = parse(html)
-        print(results)
-        send_data
+
+        sent_data_tg = send_data_to_tg(results,sent_data_tg)
+        sent_data_db = send_data_to_db(results,sent_data_db)
+
         time.sleep(10)
 
 
--- rep/tg.py
+++ rep/tg.py
@@ -1,5 +1,9 @@
 import telebot
 bot = telebot.TeleBot('1499593962:AAG5ObbcZsycM9O6M9wHD91Xi05b_mPsbTg')
 
-def send_data():
-	bot.send_message(1340073704, 'Wake up!')
+def send_data_to_tg(results, sent_data_tg):
+	for i in range(len(results)):
+		if results[i] not in sent_data_tg:
+			bot.send_message(1340073704, results[i])
+			sent_data_tg.append(results[i])
+	return(sent_data_tg)
