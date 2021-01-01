3e909cf81b30d07bff08c16b19d8a768654fd8b0
uebermensch1488 <lib.ballmerpeak@gmail.com>
2021-01-01 21:39:44 +0300

    prefinal

--- rep/tg.py
+++ rep/tg.py
@@ -4,12 +4,21 @@ from date import day_add
 
 bot = telebot.TeleBot("1499593962:AAG5ObbcZsycM9O6M9wHD91Xi05b_mPsbTg")
 
-bot.send_message(1340073704, "I HATE NIGGERS")
-
 def send_data_to_tg(results, sent_data_tg):
     for disconnection in results:
         if disconnection not in sent_data_tg:
-            bot.send_message(1340073704, disconnection)
+            bot.send_message(1340073704, f'''
+                {disconnection['region']}\n
+                {disconnection['district']}\n
+                {disconnection['location']}\n
+                {disconnection['object']}\n
+                {disconnection['disconn-start-date']}\n
+                {disconnection['disconn-start-time']}\n
+                {disconnection['disconn-end-date']}\n
+                {disconnection['disconn-end-time']}\n
+                {disconnection['branch']}\n
+                {disconnection['res_title']}
+                ''')
             sent_data_tg.append(disconnection)
     return sent_data_tg
 
