5276a9fc19ba0d2b39ef364596b2182d05f18535
uebermensch1488 <lib.ballmerpeak@gmail.com>
2021-01-03 08:38:28 +0300

    nigga

--- rep/tg.py
+++ rep/tg.py
@@ -7,17 +7,7 @@ bot = telebot.TeleBot("1499593962:AAG5ObbcZsycM9O6M9wHD91Xi05b_mPsbTg")
 def send_data_to_tg(results, sent_data_tg):
     for disconnection in results:
         if disconnection not in sent_data_tg:
-            bot.send_message(-497887973, f'''
-                {disconnection['region']}\n
-                {disconnection['district']}\n
-                {disconnection['location']}\n
-                {disconnection['object']}\n
-                {disconnection['disconn-start-date']}\n
-                {disconnection['disconn-start-time']}\n
-                {disconnection['disconn-end-date']}\n
-                {disconnection['disconn-end-time']}\n
-                {disconnection['branch']}\n
-                {disconnection['res_title']}
+            bot.send_message(1340073704, f'''Населённый пункт: {disconnection['location']}\n\nУлица: {disconnection['object']}\n\nДата начала отключения: {disconnection['disconn-start-date']}\n\nВремя начала отключения: {disconnection['disconn-start-time']}\n\nДата восстановления энергоснабжения: {disconnection['disconn-end-date']}\n\nВремя восстановления энергоснабжения:{disconnection['disconn-end-time']}\n\nФилиал: {disconnection['branch']}\n\nРЭС: {disconnection['res_title']}
                 ''')
             sent_data_tg.append(disconnection)
     return sent_data_tg
@@ -26,10 +16,10 @@ def check_disconnection(results):
     dates = day_add()
     for disconnection in results:
         if disconnection["disconn-start-date"] == f'{dates[0]}' and disconnection["region"] == "д. Сидорово":
-            bot.send_message(-497887973, "Завтра планируется отключение в деревне Сидорово")
+            bot.send_message(1340073704, "Завтра планируется отключение в деревне Сидорово")
         
         if disconnection["disconn-start-date"] == f'{dates[1]}' and disconnection["region"] == "д. Сидорово":
-            bot.send_message(-497887973, "Через три дня планируется отключение в деревне Сидорово")
+            bot.send_message(1340073704, "Через три дня планируется отключение в деревне Сидорово")
         
         if disconnection["disconn-start-date"] == f'{dates[2]}' and disconnection["region"] == "д. Сидорово":
-            bot.send_message(-497887973, "Через неделю планируется отключение в деревне Сидорово")
+            bot.send_message(1340073704, "Через неделю планируется отключение в деревне Сидорово")
