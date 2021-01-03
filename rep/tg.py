57bf52f8ae514e3c94308a8d1f7a286a8099bafa
Akhmetov Aslan <lib.ballmerpeak@gmail.com>
2021-01-03 08:12:13 +0300

    SORRY FOR HATING FUUCKING NIGGERS

--- rep/tg.py
+++ rep/tg.py
@@ -7,7 +7,7 @@ bot = telebot.TeleBot("1499593962:AAG5ObbcZsycM9O6M9wHD91Xi05b_mPsbTg")
 def send_data_to_tg(results, sent_data_tg):
     for disconnection in results:
         if disconnection not in sent_data_tg:
-            bot.send_message(1340073704, f'''
+            bot.send_message(-497887973, f'''
                 {disconnection['region']}\n
                 {disconnection['district']}\n
                 {disconnection['location']}\n
@@ -26,10 +26,10 @@ def check_disconnection(results):
     dates = day_add()
     for disconnection in results:
         if disconnection["disconn-start-date"] == f'{dates[0]}' and disconnection["region"] == "д. Сидорово":
-            bot.send_message(1340073704, "Завтра планируется отключение в деревне Сидорово")
+            bot.send_message(-497887973, "Завтра планируется отключение в деревне Сидорово")
         
         if disconnection["disconn-start-date"] == f'{dates[1]}' and disconnection["region"] == "д. Сидорово":
-            bot.send_message(1340073704, "Через три дня планируется отключение в деревне Сидорово")
+            bot.send_message(-497887973, "Через три дня планируется отключение в деревне Сидорово")
         
         if disconnection["disconn-start-date"] == f'{dates[2]}' and disconnection["region"] == "д. Сидорово":
-            bot.send_message(1340073704, "Через неделю планируется отключение в деревне Сидорово")
+            bot.send_message(-497887973, "Через неделю планируется отключение в деревне Сидорово")
